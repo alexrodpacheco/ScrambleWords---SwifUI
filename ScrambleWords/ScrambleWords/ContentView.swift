d3d64dbcf54c64a4b26cd640a00439cb6175e31a
Alex Pacheco <alexrodpacheco@gmail.com>
2025-08-18 17:10:18 -0300

    creating user interface

--- ScrambleWords/ScrambleWords/ContentView.swift
+++ ScrambleWords/ScrambleWords/ContentView.swift
@@ -1,16 +1,25 @@
-//
-//  ContentView.swift
-//  ScrambleWords
-//
-//  Created by Alex Rodrigues Pacheco on 18/08/25.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        GeometryReader { proxy in
+            ZStack {
+                Color.black.ignoresSafeArea()
+                VStack {
+                    VStack {
+                        Image("orange")
+                            .resizable()
+                            .frame(width: 100, height: 100)
+                    }
+                    .frame(width: proxy.size.width * 0.9, height: proxy.size.width * 0.9)
+                    .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color.gray, lineWidth: 2))
+                    Text("Score 0")
+                        .font(.system(size: 15))
+                        .foregroundColor(Color.white)
+                        .padding(.top)
+                }
+            }
+        }
     }
 }
 
