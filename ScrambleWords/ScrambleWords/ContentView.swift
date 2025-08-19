dfdf22b1ab20f71bc744259bfdcbfe9120db5d65
Alex Pacheco <alexrodpacheco@gmail.com>
2025-08-19 09:34:20 -0300

    add guessed letters

--- ScrambleWords/ScrambleWords/ContentView.swift
+++ ScrambleWords/ScrambleWords/ContentView.swift
@@ -1,15 +1,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var letters: [String] = ["O", "R", "A", "N", "G", "E", ]
+    @State var guessedLetters: [String] = []
+    
     var body: some View {
         GeometryReader { proxy in
             ZStack {
                 Color.black.ignoresSafeArea()
                 VStack {
                     VStack {
+                        Spacer()
                         Image("orange")
                             .resizable()
                             .frame(width: 100, height: 100)
+                        Spacer()
+                        HStack {
+                            ForEach(guessedLetters, id: \.self) { guessedLetter in
+                                VStack {
+                                    LetterView(letter: guessedLetter)
+                                    Rectangle()
+                                        .fill(Color.white)
+                                        .frame(width: 25, height: 2)
+                                }
+                            }
+                        }
+                        .padding(.bottom, 20)
                     }
                     .frame(width: proxy.size.width * 0.9, height: proxy.size.width * 0.9)
                     .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color.gray, lineWidth: 2))
@@ -18,9 +35,14 @@ struct ContentView: View {
                         .foregroundColor(Color.white)
                         .padding(.top)
                     HStack {
-                        LetterView(letter: "A")
-                            .padding(.top)
+                        ForEach(letters, id: \.self) { letter in
+                            LetterView(letter: letter)
+                                .onTapGesture {
+                                    guessedLetters.append(letter)
+                                }
+                        }
                     }
+                    .padding(.top)
                 }
             }
         }
