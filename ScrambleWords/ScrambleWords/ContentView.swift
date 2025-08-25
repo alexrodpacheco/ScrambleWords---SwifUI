250979cb0e3bc2c7cc281ca772cd2317c7f10832
Alex Pacheco <alexrodpacheco@gmail.com>
2025-08-25 16:46:13 -0300

    cheking if the guess is right or wrong

--- ScrambleWords/ScrambleWords/ContentView.swift
+++ ScrambleWords/ScrambleWords/ContentView.swift
@@ -11,6 +11,7 @@ struct ContentView: View {
         Letter(id: 5, text: "A")
     ]
     @State var guessedLetters: [Letter] = []
+    let correctAnwser = "ORANGE"
     
     var body: some View {
         GeometryReader { proxy in
@@ -52,6 +53,17 @@ struct ContentView: View {
                                     if !letter.text.isEmpty {
                                         guessedLetters.append(letter)
                                         letters[index].text = ""
+                                        if guessedLetters.count == letters.count {
+                                            var guessedAnswer = ""
+                                            for guessedLetter in guessedLetters {
+                                                guessedAnswer += guessedLetter.text
+                                            }
+                                            if guessedAnswer == correctAnwser {
+                                                print("correct")
+                                            } else {
+                                                print("wrong")
+                                            }
+                                        }
                                     }
                                 }
                         }
